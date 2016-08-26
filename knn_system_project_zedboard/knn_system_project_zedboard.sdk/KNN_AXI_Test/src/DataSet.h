d0c2d9dc33cd14268e4dfb4f8243a7c096f248c3
Dakota01011 <dakotakoelling@gmail.com>
2016-08-26 12:54:04 -0500

    Zedboard version
    First working commit of the zedboard version

---
+++ knn_system_project_zedboard/knn_system_project_zedboard.sdk/KNN_AXI_Test/src/DataSet.h
@@ -0,0 +1,21 @@
+/*
+ * DataSet.h
+ *
+ *  Created on: Jul 20, 2016
+ *      Author: dkoellin
+ */
+
+#ifndef SRC_DATASET_H_
+#define SRC_DATASET_H_
+
+#define NUM_FEATURES 11
+#define NUM_POINTS 11
+
+class DataSet {
+public:
+	DataSet();
+	void getMyIntDataSet(int pArray[NUM_POINTS][NUM_FEATURES]);
+	virtual ~DataSet();
+};
+
+#endif /* SRC_DATASET_H_ */
---
+++ knn_system_project_zedboard/knn_system_project_zedboard.srcs/sources_1/bd/design_1/ipshared/dakota/knn_accelerator_v1_41/hdl/distanceCalcAcc.v
@@ -0,0 +1,86 @@
+// Dakota Koelling
+
+module distanceCalculationAccumulator #(
+	parameter DATA_WIDTH = 32,
+	parameter DIMENSIONS = 32,
+	parameter VAL_WIDTH = 32
+) (
+	input clk,
+	input reset,
+	input wr_en,
+	input dataIn_Valid,
+	input done, 
+	input signed [DATA_WIDTH-1:0] data1,
+	input signed [DATA_WIDTH-1:0] data2,
+	output reg [VAL_WIDTH-1:0] distance,
+	output reg distanceValid
+);
+
+localparam dif_width = DATA_WIDTH+1;
+localparam sqr_width = dif_width*2;
+localparam acc_width = sqr_width+DIMENSIONS;
+
+(* mark_debug = "true" *)	reg [dif_width-1:0] difference;
+(* mark_debug = "true" *)	reg [sqr_width-1:0] squared;
+(* mark_debug = "true" *)	reg [acc_width-1:0] accumulator;
+(* mark_debug = "true" *)	reg stop;
+(* mark_debug = "true" *)	integer i;
+
+	always @(posedge clk)
+	begin : proc_stop
+		if(reset)
+		begin
+			stop <= 0;
+		end
+		else
+		begin
+			if(wr_en && done && distanceValid)
+			begin
+				// distanceValid can go high once after done then it will stop
+				stop <= 1;
+			end
+		end
+	end
+
+	always @(posedge clk)
+	begin
+		if (reset || stop)
+		begin
+			difference <= 0;
+			squared <= 0;
+			accumulator <= 0;
+			i <= -3;
+			distanceValid <= 0;
+			distance <= 0;
+		end
+		else 
+		begin
+			if(wr_en && dataIn_Valid)
+			begin
+				if(data1 > data2)
+				begin
+					difference <= data1 - data2;
+				end
+				else
+				begin
+					difference <= data2 - data1;
+				end
+				squared <= difference * difference;
+				if (i >= DIMENSIONS-1)
+				begin
+					accumulator <= squared;
+					i <= 0;
+					distanceValid <= 1;
+					distance <= accumulator;
+				end
+				else
+				begin
+					accumulator <= accumulator + squared;
+					i <= i + 1;
+					distanceValid <= 0;
+				end
+			end
+		end
+	end
+
+endmodule
